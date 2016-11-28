2d87d053a68af963dc4b1bf1683b25d9486a98fc
mike-a-yen <mike.a.yen@gmail.com>
2016-11-28 13:52:16 -0800

    tests

---
+++ test.py
@@ -0,0 +1,26 @@
+import unittest
+from CalCalc import calculate
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calculate('1+2'),'3')
+
+    def test_subtract(self):
+        self.assertEqual(calculate('0-1'),'-1')
+
+    def test_div_by_zero(self):
+        self.assertEqual(calculate('1/0'),'1/0')
+
+    def test_multiply(self):
+        self.assertEqual(calculate('10*10'),'100')
+
+    def test_silly(self):
+        self.assertEqual(calculate('diameter of earth in lightyears'),
+                         '1.3468323×10^-9 ly  (light years)')
+        
+    def test_question(self):
+        self.assertEqual(calculate('mass of moon in kg'),'7.3459×10^22 kg  (kilograms)')
+
+
+if __name__ == '__main__':
+        unittest.main()
